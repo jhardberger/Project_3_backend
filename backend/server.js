2cc10c6bff0ce2df29598b94cd921e101c2d400b
John <john.hardberger@gmail.com>
2018-11-05 09:59:44 -0600

    file structure basic

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express 		= require('express');
+const app			= express();
+const PORT 			= process.env.PORT || 3000;
+
+//API CONNECTOR
+
+//MIDDLEWARE
+
+//REQUIRE CONTROLLERS
+
+//USE MIDDLEWARE
+
+app.listen(PORT, () => {
+	console.log('listening on port: ' + PORT);
+})
